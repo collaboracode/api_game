96e6cac6c86f68c93c11377a00c3f4378ca00c7c
crazyDog <alarias1@outlook.com>
2024-05-11 15:07:54 -0500

    syntax edit

--- playerStatistics/playerScenarios.js
+++ playerStatistics/playerScenarios.js
@@ -8,3 +8,8 @@ const playerDebuffScenarios = [
     {scenario: "Player is shot by a gun", effect: health - 1}
 
 ]
+
+//Create either a random character generator or a class based system; something that pre-assigns points to each of the special attributes
+//Follow soup's example of making an API call. Try to make an API call. 
+
+//Look up unreal map generation examples. Something a bit older to start off with UR3 perhaps first. 
